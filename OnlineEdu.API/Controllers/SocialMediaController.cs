feb6c00b49862c7df49417767b21ab8d320f8e7a
Mehmet Can KALABAŞ <mehmetcankalabas.mk@gmail.com>
2025-01-21 13:55:08 +0300

    Controllers and DTOs have been created.

---
+++ OnlineEdu.API/Controllers/SocialMediaController.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using OnlineEdu.Business.Abstract;
+using OnlineEdu.DTO.DTOs.MessageDtos;
+using OnlineEdu.DTO.DTOs.SocialMediaDtos;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocialMediaController(IGenericService<SocialMedia> _socialMediaService, IMapper _mapper) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var values = _socialMediaService.TGetList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var value = _socialMediaService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _socialMediaService.TDelete(id);
+            return Ok("Sosyal Medya Alanı Silindi.");
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateSocialMediaDto createSocialMediaDto)
+        {
+            var newValue = _mapper.Map<SocialMedia>(createSocialMediaDto);
+            _socialMediaService.TCreate(newValue);
+            return Ok("Yeni Sosyal Medya Alanı Oluşturuldu.");
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateSocialMediaDto updateSocialMediaDto)
+        {
+            var newValue = _mapper.Map<SocialMedia>(updateSocialMediaDto);
+            _socialMediaService.TUpdate(newValue);
+            return Ok("Sosyal Medya Alanı Güncellendi.");
+        }
+
+    }
+}
