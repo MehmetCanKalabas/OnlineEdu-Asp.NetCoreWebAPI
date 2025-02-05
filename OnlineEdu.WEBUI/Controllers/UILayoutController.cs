94f95cb83ac341308986f2a5b97c7e94f2d0779e
Mehmet Can KALABAŞ <mehmetcankalabas.mk@gmail.com>
2025-02-05 10:20:02 +0300

    ViewComponents yapısı oluşturuldu ve ekleniyor.(The ViewComponents structure has been created and is being added)

---
+++ OnlineEdu.WEBUI/Controllers/UILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEdu.WEBUI.Controllers
+{
+    public class UILayoutController : Controller
+    {
+        public IActionResult Layout()
+        {
+            return View();
+        }
+    }
+}
---
+++ OnlineEdu.WEBUI/ViewComponents/Home/_HomeBannerComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEdu.WEBUI.ViewComponents.Home
+{
+    public class _HomeBannerComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
