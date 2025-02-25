b657a30b456d85e9e3a397632256941f59510018
Mehmet Can KALABAŞ <mehmetcankalabas.mk@gmail.com>
2025-02-25 11:26:56 +0300

    Ana sayfa componentler olarak oluşturuldu(Home page created as components)

--- OnlineEdu.WEBUI/ViewComponents/Home/_HomeCourseCategoryComponent.cs
+++ OnlineEdu.WEBUI/ViewComponents/Home/_HomeCourseCategoryComponent.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using OnlineEdu.WEBUI.DTOs.BannerDtos;
+using OnlineEdu.WEBUI.DTOs.CourseCategoryDtos;
 using OnlineEdu.WEBUI.Helpers;
 
 namespace OnlineEdu.WEBUI.ViewComponents.Home
 {
-    public class _HomeBannerComponent : ViewComponent
+    public class _HomeCourseCategoryComponent : ViewComponent
     {
         private readonly HttpClient _client = HttpClientInstance.CreateClient();
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            var values = await _client.GetFromJsonAsync<List<ResultBannerDto>>("banners");
+            var values = await _client.GetFromJsonAsync<List<ResultCourseCategoryDto>>("courseCategories/GetActiveCategories");
             return View(values);
         }
     }
--- OnlineEdu.WEBUI/ViewComponents/Home/_HomeCourseComponent.cs
+++ OnlineEdu.WEBUI/ViewComponents/Home/_HomeCourseComponent.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using OnlineEdu.WEBUI.DTOs.BannerDtos;
+using OnlineEdu.WEBUI.DTOs.CourseDtos;
 using OnlineEdu.WEBUI.Helpers;
 
 namespace OnlineEdu.WEBUI.ViewComponents.Home
 {
-    public class _HomeBannerComponent : ViewComponent
+    public class _HomeCourseComponent : ViewComponent
     {
         private readonly HttpClient _client = HttpClientInstance.CreateClient();
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            var values = await _client.GetFromJsonAsync<List<ResultBannerDto>>("banners");
+            var values = await _client.GetFromJsonAsync<List<ResultCourseDto>>("courses/GetActiveCourses");
             return View(values);
         }
     }
